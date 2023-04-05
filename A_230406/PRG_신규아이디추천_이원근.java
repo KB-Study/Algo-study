0ae4f4e23c909a80ce0383e30214084c41173631
Won Keun Lee <76688104+lwg1421@users.noreply.github.com>
2023-04-05 23:27:02 +0900

    feat : 프로그래머스 신규아이디추천 이원근

--- A_230406/PRG_신규아이디추천_이원근.java
+++ A_230406/PRG_신규아이디추천_이원근.java
@@ -1,23 +1,21 @@
-package codingtest;
-
-import java.util.Scanner;
-
 //단계
-//1. 대문자 -> 소문자로
-//2. 소문자,숫자, -, _, .만 남기고 다 제거
-//3. ..은 .으로 치환
-//4. .이 처음이나 끝에 있으면 제거
-//5. id가 빈 문자열이면 a 대입
-//6. 16자 이상이면 앞에서 15글자까지만 자름
-//7. .이 끝에 위치하면 . 제거
-//8. id가 2자 이하라면 마지막 문자를 길이가 3이 될 때 까지 반복
+// 1. 대문자 -> 소문자로
+// 2. 소문자,숫자, -, _, .만 남기고 다 제거
+// 3. ..은 .으로 치환
+// 4. .이 처음이나 끝에 있으면 제거
+// 5. id가 빈 문자열이면 a 대입
+// 6. 16자 이상이면 앞에서 15글자까지만 자름
+// 7. .이 끝에 위치하면 . 제거
+// 8. id가 2자 이하라면 마지막 문자를 길이가 3이 될 때 까지 반복
 
-public class P_신규아이디추천 {
 
-	public static void main(String[] args) {
-		String answer = "z-+.^.";
-				
-		 // 1. 대문자 -> 소문자로
+class Solution {
+    public String solution(String new_id) {
+        
+	String answer = new_id;
+		
+		
+	// 1. 대문자 -> 소문자로
         answer = answer.toLowerCase();
         
         //2. 소문자,숫자, -, _, .만 남기고 다 제거
@@ -44,9 +42,8 @@ public class P_신규아이디추천 {
         	} else answer = "a";
         }
         
-        
         //5. id가 빈 문자열이면 a 대입
-        if(answer.equals("")) {
+        if(answer.length() == 0) {
         	answer = "a";
         }
         
@@ -64,7 +61,8 @@ public class P_신규아이디추천 {
         while(answer.length() <= 2) {
         	answer = answer + answer.charAt(answer.length()-1);
         }
-
-	}
-
+	
+        
+        return answer;
+    }
 }
