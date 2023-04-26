40c05923da30500bb41efa520d4d5aa448a575a6
LisaJeoung <jlisa9912234567890@gmail.com>
2023-04-26 20:23:48 +0900

    mm

--- A_230424/BOJ2467_용액_정승혜.java
+++ A_230424/BOJ2467_용액_정승혜.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ2467_용액 {
+public class BOJ2467_용액_정승혜 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
