1b0eea1694f7c490244ebb719c768ab13d90ca1f
im-shung <imshung00@gmail.com>
2023-07-19 01:02:26 +0900

    230719 임서영

---
+++ A_230719/PRG_롤케이크자르기_임서영.java
@@ -0,0 +1,31 @@
+package A_230719;
+
+import java.util.*;
+
+public class PRG_롤케이크자르기_임서영 {
+    class Solution {
+        public int solution(int[] topping) {
+            HashSet<Integer> hs = new HashSet<>();
+            int n = topping.length;
+            int[] a1 = new int[n];
+            int[] a2 = new int[n];
+
+            for(int i=0; i<n; i++){
+                hs.add(topping[i]);
+                a1[i] = hs.size();
+            }
+            hs.clear();
+            for(int i=n-1; i>-1; i--){
+                hs.add(topping[i]);
+                a2[i] = hs.size();
+            }
+            int answer = 0;
+            for(int i=0; i<n-1; i++){
+                if(a1[i] == a2[i+1]){
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ A_230719/PRG_모음사전_임서영.java
@@ -0,0 +1,26 @@
+package A_230719;
+
+import java.util.*;
+
+public class PRG_모음사전_임서영 {
+    class Solution {
+        String[] arr = {"A", "E", "I", "O", "U"};
+        HashMap<String, Integer> dic = new HashMap<>();
+        int order = 0;
+
+        public int solution(String word) {
+
+            fuc("", 0);
+
+            return dic.get(word);
+        }
+
+        void fuc(String word, int length) {
+            if (length > 5) return;
+            dic.put(word, order++);
+            for(String ch : arr) {
+                fuc(word + ch, length + 1);
+            }
+        }
+    }
+}
