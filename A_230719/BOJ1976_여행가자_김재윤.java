a326f25cd0da8369bc42735d456f00dfa5857442
JaeYun Kim <84389492+jaeyun1723@users.noreply.github.com>
2023-07-19 13:19:13 +0900

    230719 김재윤

---
+++ A_230719/BOJ1976_여행가자_김재윤.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1976_여행가자_김재윤 {
+
+	static int[] info;
+
+	static int find(int v) {
+		if (info[v] == v)
+			return v;
+		else
+			return info[v] = find(info[v]);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int[][] city = new int[N + 1][N + 1];
+		info = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+
+		for (int i = 1; i <= N; i++)
+			info[i] = i;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				city[i][j] = Integer.parseInt(st.nextToken());
+				if (city[i][j] == 1) {
+					int pari = find(i);
+					int parj = find(j);
+					if (pari > parj)
+						info[pari] = parj;
+					else
+						info[parj] = pari;
+				}
+			}
+		}
+		boolean flag = false;
+		st = new StringTokenizer(br.readLine());
+		int[] plan = new int[M];
+		for (int i = 0; i < M; i++)
+			plan[i] = Integer.parseInt(st.nextToken());
+		for (int i = 1; i < M; i++) {
+			if (info[plan[i - 1]] != info[plan[i]]) {
+				flag = true;
+				break;
+			}
+		}
+		if (flag)
+			System.out.println("NO");
+		else
+			System.out.println("YES");
+
+	}//
+
+}//
---
+++ A_230719/PRG_모음사전_김재윤.java
@@ -0,0 +1,23 @@
+class PRG_모음사전_김재윤 {
+    static String[] vowel={"A","E","I","O","U"};
+    static int answer=0, cnt=0;
+    static String target;
+    
+    static void dictionary(String word){
+      if(target.equals(word)){
+          answer=cnt;
+          return;
+      }
+      if(word.length()>=5||answer!=0)
+          return;
+        for(int i=0;i<vowel.length;i++){
+            cnt++;
+            dictionary(word+vowel[i]);
+        }
+    }
+    public int solution(String word) {
+        target=word;
+        dictionary("");
+        return answer;
+    }
+}
